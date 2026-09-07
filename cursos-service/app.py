a468f1978ceb6f519ed8797a6fd77e8fbfb9d458
dayanfajardo <145393403+dayanfajardo@users.noreply.github.com>
2026-09-06 19:18:25 -0500

    Update app.py

--- cursos-service/app.py
+++ cursos-service/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def inicio():
-    return "¡Hola! Desde el servicio de cursos 🚀"
+    return "¡Hola! Desde el servicio de cursos "
 
 
 if __name__ == "__main__":
